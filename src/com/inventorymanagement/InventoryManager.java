f10708f7149fa906af2d8d234edefd5627df4b8b
anirudha mayya <anirudha.mayya@gmail.com>
2021-09-08 22:45:21 +0530

    Added solution to question 2

---
+++ src/com/inventorymanagement/InventoryManager.java
@@ -0,0 +1,82 @@
+package com.inventorymanagement;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class InventoryManager {
+	static Map<String,Double> map = new HashMap<>();
+	public static void inventoryManager() {
+		List<JSONObject> list = inventoryFactory();
+		
+		for(JSONObject obj : list) {
+			String type=(String) obj.get("type");
+			String name=(String) obj.get("name");
+			double weight=(double) obj.get("weight");
+			double pricePerKg = (double) obj.get("priceperKg");
+			double totalPrice = pricePerKg*weight;
+			map.put(name, totalPrice);
+		}
+		
+		displayJSON();
+		
+	}
+	private static void displayJSON() {
+		JSONArray array =  new JSONArray();
+		for (String s: map.keySet()) {
+			//object1 = new JSONObject();
+			JSONObject object1=new JSONObject();
+			object1.put("name", s);
+			object1.put("Totalprice",map.get(s));
+			array.add(object1);
+		}
+		//System.out.println(array);
+		JSONObject mainObject =  new JSONObject();
+		mainObject.put("Result", array);
+		System.out.println(mainObject.toJSONString());
+		
+	}
+	private static List<JSONObject> inventoryFactory() {
+	
+		List<JSONObject> list = new ArrayList<JSONObject>();
+		JSONParser parser = new JSONParser();
+		try {
+			Reader reader = new FileReader("/Users/anirudhasm/Desktop/eclipse-yml_training_workspace/InventoryManagement/data/inventory.json");
+			JSONObject inventory = (JSONObject) parser.parse(reader);
+			JSONArray array= (JSONArray) inventory.get("inventory");
+			Iterator<JSONObject> iterator = array.iterator();
+			while(iterator.hasNext()) {
+				JSONObject object2=iterator.next();
+				list.add(object2);
+			}
+			
+			
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (org.json.simple.parser.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return list;
+	}
+	public static void main(String args[]) {
+		InventoryManager.inventoryManager();
+	}
+
+}
+
--- src/com/inventorymanagement/Main.java
+++ src/com/inventorymanagement/Main.java
@@ -1,6 +1,8 @@
 package com.inventorymanagement;
 
+import java.io.File;
 import java.io.FileReader;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
@@ -16,24 +18,29 @@ import org.json.simple.parser.ParseException;
 
 public class Main {
 
-	static Map<String, Double> map = new HashMap<>();
 	public static void main(String[] args) {
-		JSONParser jsonParser = new JSONParser();
+		JSONObject jsonObject1 = new JSONObject();
+		JSONObject jsonObject2 = new JSONObject();
+		JSONParser parser = new JSONParser();
+		Map<String,Double> map = new HashMap<>();
 		try {
 			Reader reader = new FileReader("/Users/anirudhasm/Desktop/eclipse-yml_training_workspace/InventoryManagement/data/inventory.json");
-			JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
-			JSONArray array = (JSONArray) jsonObject.get("inventory");
-			Iterator<JSONObject> iterator = array.iterator();
-			
+			JSONArray array = new JSONArray();
+			jsonObject1 = (JSONObject) parser.parse(reader);
+			array =(JSONArray) jsonObject1.get("inventory");
+			Iterator<JSONObject> iterator = array.iterator(); //iterate values in array
+
 			while(iterator.hasNext()) {
-				JSONObject jsonObject2 = iterator.next();
+				jsonObject2 = iterator.next();
 				String name = (String) jsonObject2.get("name");
+				String type=(String) jsonObject2.get("type");
 				double price = (double) jsonObject2.get("priceperKg");
 				double weight = (double) jsonObject2.get("weight");
 				System.out.println("Name : " + name + " Price : " + price + "Weight :"+ weight);
-				
-				map.put(name, (price*weight));
+
+				map.put(name, (price*weight)); //get total value
 			}
+			System.out.println(map);
 
 		} catch (IOException | ParseException e) {
 			e.printStackTrace();
@@ -41,6 +48,10 @@ public class Main {
 		writeJson(map);
 	}
 
+	/**
+	 * @param map
+	 * Write into file
+	 */
 	public static void writeJson(final Map<String,Double> map) {
 		JSONArray array = new JSONArray();
 
@@ -52,22 +63,25 @@ public class Main {
 			array.add(object);
 
 		}
-
 		JSONObject mainObject =  new JSONObject();
 		mainObject.put("results", array);
+		System.out.println(mainObject);
 
 
 		FileWriter writer;
 		try {
+			File file = new File("/Users/anirudhasm/Desktop/eclipse-yml_training_workspace/InventoryManagement/data/result.json");
+			if(file.createNewFile()) {
+
+			}
+			else {
+				System.out.println("already file exits");
+			}
 			writer = new FileWriter("/Users/anirudhasm/Desktop/eclipse-yml_training_workspace/InventoryManagement/data/result.json");
 			writer.write(mainObject.toJSONString());
 			writer.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		System.out.println(mainObject);
-		
-
 	}
 }	
